1c98773de4f0db6724bd6139005ec64b04bb9772
huzhen813 <751590100@qq.com>
2017-02-11 22:53:48 +0800

    重新设置界面样式
    整体宽度变小，search垂直居中，小图标替换，添加单曲循环和列表循环功能切换

--- audio.js
+++ audio.js
@@ -5,6 +5,7 @@ var pauseIcon = dqs('#id-icon-pause')
 var nextIcon = dqs('#id-icon-play-forward')
 var preIcon = dqs('#id-icon-play-back')
 var musicCover = dqs('.music-cover')
+var volumeMuteIcon = dqs('#id-icon-volume-mute')
 var currentTime = dqs('#id-current-time')
 var totalTime = dqs('#id-total-time')
 var playedBar = dqs('#id-played-bar')
@@ -13,29 +14,57 @@ var playList = dqs('.play-list')
 var playLists = dqsa('.play-list-song')
 var musicName = dqs('.music-name')
 var songName = dqsa('.song-name')
-var tr =dqsa('tr')
 var informationName = dqs('.information-name')
 var informationAuthor = dqs('.information-author')
-var songArtist = dqsa('song-artist')
+var songArtist = dqsa('.song-artist')
+var platePast = dqs('.plate-past')
+var plateNow = dqs('.plate-now')
+var plateNext = dqs('.plate-next')
+var totalVolume = dqs('#total-volume')
+var currentVolume = dqs('#current-volume')
+var volumeIcon = dqs('#id-icon-volume')
+var listCircleIcon = dqs('#id-icon-listcircle')
+var singleCircleIcon = dqs('#id-icon-singlecircle')
 
-
+var findMusic = function () {
+    for (var i = 0; i < songName.length; i++) {
+        var currentMusicIndex = 0
+        var a = informationName.innerText
+        var b = songName[i].innerText
+        if (b == a) {
+            var currentMusicIndex = i % songName.length
+            var pastMusicIndex = (currentMusicIndex - 1 + songName.length) % songName.length
+            var nextMusicIndex = (currentMusicIndex + 1) % songName.length
+            var pastMusicName = songName[pastMusicIndex].innerText
+            var nextMusicName = songName[nextMusicIndex].innerText
+            var nowMusicName = songName[currentMusicIndex].innerText
+            var nowAuthorName = songArtist[currentMusicIndex].innerText
+            var song = "music\\" + nowMusicName + '.mp3'
+            var cover = "cover\\" + nowMusicName + '.jpg'
+            var pastCover = "cover\\" + pastMusicName + '.jpg'
+            var NextCover = "cover\\" + nextMusicName + '.jpg'
+            informationName.innerText = nowMusicName
+            informationAuthor.innerText = nowAuthorName
+            platePast.src = pastCover
+            plateNow.src = cover
+            plateNext.src = NextCover
+            musicCover.src = cover
+            music.src = song
+            break
+        }
+    }
+}
 var musicPlay = function(){
     music.play()
     playIcon.classList.add('hidden')
     pauseIcon.classList.remove('hidden')
     musicCover.classList.add('rotated')
-    // toggleClass(playIcon, 'hidden')
-    // toggleClass(pauseIcon, 'hidden')
-    // toggleClass(musicCover, 'rotated')
 }
 var musicPause = function(){
     music.pause()
     playIcon.classList.remove('hidden')
     pauseIcon.classList.add('hidden')
     musicCover.classList.remove('rotated')
-    // toggleClass(pauseIcon, 'hidden')
-    // toggleClass(playIcon, 'hidden')
-    // toggleClass(musicCover, 'rotated')
 }
 playIcon.addEventListener('click', musicPlay)
 pauseIcon.addEventListener('click', musicPause)
@@ -59,7 +88,16 @@ setInterval(function updatePlayedBar (){
         nextIcon.click()
     }
 }, 1000)
-
+listCircleIcon.addEventListener('click', function() {
+    singleCircleIcon.classList.remove('hidden')
+    listCircleIcon.classList.add('hidden')
+    music.loop = true
+})
+singleCircleIcon.addEventListener('click', function() {
+    singleCircleIcon.classList.add('hidden')
+    listCircleIcon.classList.remove('hidden')
+    music.loop = false
+})
 // 函数直接调用
 // 设置播放进度条
 musicBar.onclick = function () {
@@ -77,6 +115,48 @@ playedBar.onclick = function () {
     playedBar.style.width = playedBarWidth + 'px'
 }
 
+// 音量控制
+var totalVolume = dqs('#id-total-volume')
+var currentVolume = dqs('#id-current-volume')
+var volumeBar = dqs('#id-volume-bar')
+volumeIcon.addEventListener('mouseover', function() {
+    // volumeBar.classList.remove('hidden')
+    volumeBar.classList.remove('unvisi')
+})
+volumeIcon.addEventListener('mouseout', function() {
+    volumeBar.classList.add('unvisi')
+})
+volumeBar.addEventListener('mouseover', function() {
+    volumeBar.classList.remove('unvisi')
+})
+volumeBar.addEventListener('mouseout', function() {
+    volumeBar.classList.add('unvisi')
+})
+volumeMuteIcon.addEventListener('click', function() {
+    volumeIcon.classList.remove('hidden')
+    volumeMuteIcon.classList.add('hidden')
+    music.muted = false
+})
+volumeIcon.addEventListener('click', function() {
+    volumeIcon.classList.add('hidden')
+    volumeMuteIcon.classList.remove('hidden')
+    music.muted = true
+})
+totalVolume.onclick = function () {
+    var totalVolumeWidth = totalVolume.clientWidth
+    var newCurrentVolume = event.offsetX / totalVolumeWidth
+    music.volume = newCurrentVolume
+    var currentVolumeWidth = newCurrentVolume * totalVolumeWidth
+    currentVolume.style.width = currentVolumeWidth + 'px'
+}
+
+currentVolume.onclick = function () {
+    var totalVolumeWidth = totalVolume.clientWidth
+    var newCurrentVolume = event.offsetX / totalVolumeWidth
+    music.volume = newCurrentVolume
+    var currentVolumeWidth = newCurrentVolume * totalVolumeWidth
+    currentVolume.style.width = currentVolumeWidth + 'px'
+}
 playList.addEventListener('click', function(event){
     var target = event.target
     if (target.classList.contains('song-name')) {
@@ -85,6 +165,7 @@ playList.addEventListener('click', function(event){
         informationName.innerText = target.innerText
         musicCover.src = cover
         music.src = song
+        findMusic()
         musicPlay()
     }
 })
@@ -106,21 +187,16 @@ var searchTitle = function(v) {
 }
 var likeIcon = '<img src="icon\\like.png" class="icon-like" style="width:16px;height:16px;" >'
 var unlikeIcon =  '<img src="icon\\unlike.png" class="icon-unlike" style="width:16px;height:16px">'
-$('.song-name').prepend(unlikeIcon)
-$('.song-name').prepend(likeIcon)
+$('.play-list-song').prepend(unlikeIcon)
+$('.play-list-song').prepend(likeIcon)
 
-// var songs = [
-//     playLists[0].attributes["path"].value,
-//     playLists[1].attributes["path"].value,
-//     playLists[2].attributes["path"].value,
-// ]
 // 音乐播放结束后播放下一首
 
 music.addEventListener('ended', function(){
     for (var i = 0; i < songName.length; i++) {
-        var b = informationName.innerText
-        var c = songName[i].innerText
-        if (c == b) {
+        var a = informationName.innerText
+        var b = songName[i].innerText
+        if (a == b) {
             // 在此处设置播放结束后下一曲的序号
             // 可以设置音乐循环模式,将orderLoop的计算方式更换一下即可
             var orderLoop = (i + 1) % songName.length
@@ -130,6 +206,7 @@ music.addEventListener('ended', function(){
             informationName.innerText = f
             musicCover.src = cover
             music.src = song
+            findMusic()
             musicPlay()
             break
         }
@@ -151,15 +228,15 @@ var changeMusic = function (direct) {
     if (music.attributes["src"] == undefined) {
         var currentSrcIndex = 0
     } else {
-                for (var i = 0; i < songName.length; i++) {
-                    var b = informationName.innerText
-                    var c = songName[i].innerText
-                    if (c == b) {
-                        var currentSrcIndex = i % songName.length
-                        break
-                    }
+        for (var i = 0; i < songName.length; i++) {
+            var b = informationName.innerText
+            var c = songName[i].innerText
+            if (c == b) {
+                var currentSrcIndex = i % songName.length
+                break
                 }
             }
+        }
     if (direct === 'next') {
         var currentSrcIndex = (currentSrcIndex + 1) % songName.length
     } else {
@@ -171,12 +248,14 @@ var changeMusic = function (direct) {
     informationName.innerText = f
     musicCover.src = cover
     music.src = song
+    findMusic()
     musicPlay()
 }
 
 // 初始化播放音量和循环
 var playInitialize = function () {
-    music.volume = 0.2
+    music.volume = 0.5
+    currentVolume.style.width = '35px'
     // music.loop = true
 }
 var _mainFunction = function(){
